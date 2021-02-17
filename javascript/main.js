5401c965a4231b3ab1af784deef95b2c5e9ae2d0
Aashiq420 <71071675+Aashiq420@users.noreply.github.com>
2021-02-17 13:46:55 +0200

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,84 @@
-// var content = "If life is so blue, then select another colour from the rainbow";
+//hamburger
+function toggleNav() {
+  const links = document.getElementById("navbar");
 
-// var ele = "<span>" + content.split("").join("</span><span>") + "</span>";
+  //   Check styles on links
+  links.style.width == "0px"
+    ? (links.style.width = "100vw")
+    : (links.style.width = "0px");
+}
 
-// $(ele)
-//   .hide()
-//   .appendTo("p")
-//   .each(function (i) {
-//     $(this)
-//       .delay(100 * i)
-//       .css({
-//         display: "inline",
-//         opacity: 0,
-//       })
-//       .animate(
-//         {
-//           opacity: 1,
-//         },
-//         100
-//       );
-//   });
+//display
+function display() {
+  if (document.getElementById("mywhatsapp").innerHTML == "Whatsapp") {
+    document.getElementById("mywhatsapp").innerHTML = "0732581571";
+  } else if (document.getElementById("mywhatsapp").innerHTML == "0732581571") {
+    document.getElementById("mywhatsapp").innerHTML = "Whatsapp";
+  }
+}
+function display2() {
+  if (document.getElementById("myemail").innerHTML == "Email") {
+    document.getElementById("myemail").innerHTML = "adams.aashiq@gmail.com";
+  } else if (
+    document.getElementById("myemail").innerHTML == "adams.aashiq@gmail.com"
+  ) {
+    document.getElementById("myemail").innerHTML = "Email";
+  }
+}
+//When the user scrolls down 800px from the top of the document, change navbar color
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 950 ||
+    document.documentElement.scrollTop > 950
+  ) {
+    document.getElementById("fullnav").style.backgroundColor = "black";
+  } else {
+    document.getElementById("fullnav").style.backgroundColor = "transparent";
+  }
+}
+// ---------------slideshow-----------------------
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+// -------------------------------small screen thing
+function openMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
